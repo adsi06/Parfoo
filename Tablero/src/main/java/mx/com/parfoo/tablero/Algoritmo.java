d7f17cbd4ddab4c355b87650acc3ad4e49a34d6a
Fernando Peña <lpeaflo1@itam.mx>
2018-05-23 08:24:01 -0500

    Added class Algoritmo
    Falta añadir la funcionalidad del algoritmo. Todo parece indicar que se recibirá un archivo JSON.

---
+++ Tablero/src/main/java/mx/com/parfoo/tablero/Algoritmo.java
@@ -0,0 +1,11 @@
+package mx.com.parfoo.tablero;
+
+import org.apache.log4j.Logger;
+
+public class Algoritmo {
+	private final static Logger LOGGER = Logger.getLogger(Algoritmo.class);
+	
+	public void algoritmo() {
+		
+	}
+}
