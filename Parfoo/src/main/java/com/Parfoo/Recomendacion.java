e6d6d6f398aa13294ec410148965e0c875fc7068
Fernando Peña <lpeaflo1@itam.mx>
2018-05-25 10:55:43 -0500

    Added class Recomendacion

---
+++ Parfoo/src/main/java/com/Parfoo/Recomendacion.java
@@ -0,0 +1,40 @@
+package com.Parfoo;
+
+/**
+ * action -> 0 = recomendation; 1 = buy/sell
+ * type -> 0 = buy; 1 -> sell;
+ *
+ */
+public class Recomendacion {
+	private int type;
+	private int action;
+
+	public Recomendacion(int type, int action) {
+		super();
+		this.type = type;
+		this.action = action;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public int getAction() {
+		return action;
+	}
+
+	@Override
+	public String toString() {
+		if(this.type == 0) {
+			if(this.action == 0)
+				return "Recomendation: Buy on Low";
+			else
+				return "Recomendation: Sell on High";
+		} else{
+			if(this.action == 0)
+				return "Action: Buy on Low";
+			else
+				return "Action: Sell on High";
+		}
+	}
+}
